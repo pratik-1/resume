87894ae85383cac3051ff90f0a4ee03cbe836978
pratik-1 <nandekarpratik04@gmail.com>
2022-04-14 14:55:03 +0100

    Update

--- resume.py
+++ resume.py
@@ -186,7 +186,7 @@ st.markdown('''
 ''')
 st.markdown('''
 - [Building Cloud Computing Solutions at Scale(4 course specialisation)](https://coursera.org/verify/specialization/JGNCBG6DUGUC) 
-- [Django for Everybody(4 course specialisation)](coursera.org/verify/specialization/3APMN2XPK5TC)
+- [Django for Everybody(4 course specialisation)](https://coursera.org/verify/specialization/3APMN2XPK5TC)
 - [Data Scientist with Python track](https://raw.githubusercontent.com/pratik-1/resume/2aa57121c825d815075e5f471c1fc2d65dbe9826/data/Data_Scientist_python_track.jpg)
 - [Data Scientist with R track](https://raw.githubusercontent.com/pratik-1/resume/2aa57121c825d815075e5f471c1fc2d65dbe9826/data/Data_Scientist_R_track.jpg)
 - [Big Data Specialist](https://raw.githubusercontent.com/pratik-1/resume/2aa57121c825d815075e5f471c1fc2d65dbe9826/data/BD_specialist.jpg)
